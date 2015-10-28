179845c78221b78504ce7eb82183ade2c04066ca
pahimar <pahimar@gmail.com>
2015-10-28 15:24:03 -0400

    Fix deserialization issue with Amazon temporary token response

--- src/main/java/com/creatubbles/api/core/Credentials.java
+++ src/main/java/com/creatubbles/api/core/Credentials.java
@@ -1,5 +1,5 @@
 package com.creatubbles.api.core;
 
-public class Creditional {
+public class Credentials {
     public String session_token, secret_access_key, access_key_id;
 }
--- src/main/java/com/creatubbles/api/response/amazon/GetAmazonTokenResponse.java
+++ src/main/java/com/creatubbles/api/response/amazon/GetAmazonTokenResponse.java
@@ -1,9 +1,9 @@
 package com.creatubbles.api.response.amazon;
 
 import com.creatubbles.api.core.CreatubblesResponse;
-import com.creatubbles.api.core.Creditional;
+import com.creatubbles.api.core.Credentials;
 
 public class GetAmazonTokenResponse extends CreatubblesResponse {
     public String error, expires_at;
-    public Creditional creditionals;
+    public Credentials credentials;
 }
