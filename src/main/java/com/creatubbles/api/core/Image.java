7f3b3df0d8d7d23a82e14b547bf4952f71e4a7c8
tterrag1098 <tterrag1098@gmail.com>
2016-01-08 16:06:53 -0500

    Add deprecated fields back to Image

--- src/main/java/com/creatubbles/api/core/Image.java
+++ src/main/java/com/creatubbles/api/core/Image.java
@@ -1,9 +1,24 @@
 package com.creatubbles.api.core;
 
+import java.util.List;
 import java.util.Map;
 
 public class Image {
-
+    /**
+     * @see #links
+     */
+    @Deprecated
+    public String url;
+    
+    @Deprecated
+    public List<Creator> creators;
+    
+    /**
+     * @see #links
+     */
+    @Deprecated
+    public String store_dir;
+    
     public Map<ImageType, String> links;
 
     public enum ImageType {
