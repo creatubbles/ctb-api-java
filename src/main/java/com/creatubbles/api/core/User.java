91d5d8d97b4c7d9ed5e5829f3b76a74f19feb8c2
tterrag1098 <tterrag1098@gmail.com>
2016-01-08 16:17:28 -0500

    Add some more fields to User

--- src/main/java/com/creatubbles/api/core/User.java
+++ src/main/java/com/creatubbles/api/core/User.java
@@ -1,8 +1,8 @@
 package com.creatubbles.api.core;
 
 public class User {
-    public String id;
-    public String username, email, country, role;
+    public String id, age, birth_month, birth_year;
+    public String username, display_name, email, country, role;
     public boolean is_teacher, is_loggable, is_site_admin, newsletter;
     public int signed_up_as;
     public String access_token;
