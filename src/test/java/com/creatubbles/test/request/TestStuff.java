4ce376872a120efdc6cff242274d50965aa38394
tterrag1098 <tterrag1098@gmail.com>
2016-06-03 20:15:53 -0400

    Remove and ignore unit tests

--- src/test/java/com/creatubbles/test/request/TestStuff.java
+++
@@ -1,9 +0,0 @@
-package com.creatubbles.test.request;
-
-
-public class TestStuff {
-    
-    public static final String CLIENT_ID = "274af44ed7c74b2daf680de6363fd9590800200c9a66483a81a0841d11e5a837";
-    public static final String CLIENT_SECRET = "a3b6ec42ce884f8eadadf5ddb9c7038c4062e707c29e4c27a44ff84c754a3485";
-    
-}
